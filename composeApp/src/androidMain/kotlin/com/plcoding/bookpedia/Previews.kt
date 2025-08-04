1613a6d4c3b2f84702de57a048d2b357c7cea769
Karcag Tamas <karcagtamas@outlook.com>
2025-08-04 23:02:16 +0200

    Book list view part 1

---
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -0,0 +1,22 @@
+package com.plcoding.bookpedia
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+
+@Preview(showBackground = true)
+@Composable
+private fun BookSearchBarPreview() {
+    MaterialTheme {
+        BookSearchBar(
+            searchQuery = "Kotlin",
+            onSearchQueryChange = {},
+            onImeSearch = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+    }
+}
