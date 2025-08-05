c961a05b62166968bf0b91f0fa408b2e54aa9a28
Karcag Tamas <karcagtamas@outlook.com>
2025-08-05 22:30:47 +0200

    Book list view part 3

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -1,12 +1,9 @@
 package com.plcoding.bookpedia
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
@@ -22,4 +19,10 @@ private fun BookSearchBarPreview() {
                 .fillMaxWidth(),
         )
     }
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+
 }
