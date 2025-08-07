14c94e2ad0aba058824563f8259944e1e77234f4
Karcag Tamas <karcagtamas@outlook.com>
2025-08-07 22:11:15 +0200

    Search books part 1

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListViewModel.kt
@@ -1,11 +1,14 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
 import androidx.lifecycle.ViewModel
+import com.plcoding.bookpedia.book.domain.BookRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class BookListViewModel : ViewModel() {
+class BookListViewModel(
+    private val bookRepository: BookRepository,
+) : ViewModel() {
 
     private val _state = MutableStateFlow(BookListState())
     val state = _state.asStateFlow()
